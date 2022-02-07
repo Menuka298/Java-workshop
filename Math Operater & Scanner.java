0d5cf68bcdd816a345fe486f3f7187e9b950db35
Menuka Dilshan <67804372+Menuka298@users.noreply.github.com>
2022-02-08 01:46:33 +0530

    Add files via upload

---
+++ Math Operater & Scanner.java
@@ -0,0 +1,32 @@
+package com.company;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+public class Main {
+
+    public static void main(String[] args) {
+
+        //math operater + , - , * , / , %
+
+        int n1 , n2 , ans;
+
+        n1 = 12;
+        n2 =4;
+
+        ans = n1 + n2;
+        System.out.println(ans);
+
+        Scanner s = new Scanner(System.in);
+        int number = s.nextInt();
+
+        System.out.println(number);
+
+        Scanner x = new Scanner(System.in);
+
+        String name = x.nextLine();
+        System.out.println(name);
+
+
+
+
+    }
+}
