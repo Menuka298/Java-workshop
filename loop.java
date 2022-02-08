4f0d150c0b23ce5e38c76c830e6277befd98a3fa
Menuka Dilshan <67804372+Menuka298@users.noreply.github.com>
2022-02-09 01:42:24 +0530

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,46 @@
+package com.company;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        //loop
+
+        //while loop
+
+    int count = 0;
+
+    while (count<5){
+        System.out.println("shanaka");
+
+        count++;
+    }
+
+
+
+    //do while loop
+
+    int x = 2;
+
+    do
+
+    {
+        System.out.println(x);
+        x++;
+    }while(x<=1);
+
+
+	//for loop
+
+        for (int count = 1; count<=10; count++){
+            System.out.println(count);
+        }
+
+        for (int count = 1; count<=10; count++){
+            System.out.println(count++);
+        }
+        
+
+    }
+}
