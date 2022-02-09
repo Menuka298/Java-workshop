b79f57007a2dfae86701df61eb4822b8ac88461e
Menuka Dilshan <67804372+Menuka298@users.noreply.github.com>
2022-02-09 20:28:16 +0530

    Add files via upload

---
+++ conditional operations.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+     //conditional operations
+
+     int num = 42;
+     System.out.println(num<30 ? "You are young " : "You are old");
+
+    }
+}
