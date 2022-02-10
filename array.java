70e348bc4bf5b80e9802e577d0ecda31eb9c77f0
Menuka Dilshan <67804372+Menuka298@users.noreply.github.com>
2022-02-10 16:26:47 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,57 @@
+package com.company;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*int num [] = new int[10];
+        num[0] = 12;
+        num[2] = 15;
+        num[9] = 54;
+
+        System.out.println(num[9]);
+
+        int num[] = {12,14,178,56};
+        System.out.println(num[2]);
+
+
+
+        int num [] = {12,14,18,15};
+        for (int i = 0 ; i<4 ; i++){
+            System.out.println(num[i]);
+        }
+
+
+        int num [] = {12,14,18,15};
+        for (int i = 0 ; i< num.length ; i++){
+            System.out.println(num[i]);
+        }
+
+
+        int num[] = {12,14,17,12,100};
+        int sum = 0;
+
+        for (int c = 0; c < num.length; c++){
+            sum+=num[c]; //sum = sum + num
+        }
+
+        System.out.println("Sum is " + sum);
+
+
+        int num[] = {2,4,8,3,7};
+        int total = 0;
+
+        for (int x : num ){
+            total+=x; //total = total + x;
+        }
+
+        System.out.println(total);
+
+         */
+
+       
+
+    }
+    
+}
